c73f716288da0b93378e7f011b7dd6dbfa37ff8a
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-08 16:25:16 -0600

    adding html and css

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,17 +2,23 @@ app.config(function ($routeProvider) {
   $routeProvider
   .when('/', {
     templateUrl: 'partials/main.html',
-    controller: 'productController'
+    controller: 'productController',
+    css: 'css/main.css'
   })
   // .when('/about', {
   //   templateUrl: 'partials/about.html'
+  //  controller: 'aboutController'
+  //   css: 'css/about.css'
   // })
   // .when('/contact', {
-  //   templateUrl: 'partials/about.html'
+  //   templateUrl: 'partials/about.html',
+  //   controller: 'aboutController',
+  //   css: 'css/about.css'
   // })
   .when('/cart', {
     templateUrl: 'partials/cart.html',
-    controller: 'cartController'
+    controller: 'cartController',
+    css: 'css/cart.css'
   })
   .otherwise('/');
 });
