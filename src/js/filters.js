b92430dcaa00e3c1ca93007362d6330de3786569
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-10 21:58:37 -0600

    started creating filter

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,2 +1,11 @@
 // create custom filter to change instock true/false to 'heck yes' or sorry, no
 'use strict';
+angular.module('shoppingCartApp')
+.filter('fixPrice', function() {
+  return function (input) {
+    var splitInput = input.split('');
+    var index = splitInput.length - 2;
+    splitInput.splice(index, 0, '.');
+    return splitInput.join();
+  };
+})
